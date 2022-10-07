0c72e2e7c9d8c27ccfdb4461527ca386135fcf3a
Elona Kajtazi <MY_NAME@example.com>
2022-10-07 15:57:55 +0200

    ...

--- src/main/java/com/pubs/pubs/Beer.java
+++ src/main/java/com/pubs/pubs/Beer.java
@@ -1,15 +1,22 @@
 package com.pubs.pubs;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+
+// import java.util.HashSet;
+// import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+// import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Beer {
@@ -21,10 +28,25 @@ public class Beer {
     public String kind;
     public Boolean isAlcoholic;
 
-    @JsonIgnore
-    @ManyToMany(mappedBy = "beers")
-    public Set<Pub> pubs = new HashSet<>();
+    // @JsonIgnore
+    // @ManyToMany(mappedBy = "beers")
+    // public Set<Pub> pubs = new HashSet<>();
 
     public Beer() {
     }
+}
+
+@RestController
+class BeerController {
+    @Autowired
+    private BeerRepo beerRepo;
+
+    @GetMapping("/beers")
+    public List<Beer> getAllBeers() {
+        return beerRepo.findAll();
+    }
+}
+
+interface BeerRepo extends JpaRepository<Beer, Integer> {
+
 }
--- src/main/java/com/pubs/pubs/Pub.java
+++ src/main/java/com/pubs/pubs/Pub.java
@@ -1,16 +1,22 @@
 package com.pubs.pubs;
 
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+import java.util.List;
+// import java.util.Set;
 
-import javax.persistence.CascadeType;
+// import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.JoinTable;
+// import javax.persistence.ManyToMany;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Entity
 public class Pub {
@@ -21,11 +27,27 @@ public class Pub {
     public String name;
     public String city;
 
-    @ManyToMany(cascade = { CascadeType.ALL })
-    @JoinTable(name = "Pub_Beer", joinColumns = { @JoinColumn(name = "pub_id") }, inverseJoinColumns = {
-            @JoinColumn(name = "beer_id") })
-    public Set<Beer> beers = new HashSet<>();
+    // @ManyToMany(cascade = { CascadeType.ALL })
+    // @JoinTable(name = "Pub_Beer", joinColumns = { @JoinColumn(name = "pub_id") },
+    // inverseJoinColumns = {
+    // @JoinColumn(name = "beer_id") })
+    // public Set<Beer> beers = new HashSet<>();
 
     public Pub() {
     }
+
+}
+
+@RestController
+class PubController {
+    @Autowired
+    private PubRepo pubRepo;
+
+    @GetMapping("/pubs")
+    public List<Pub> getAllPubs() {
+        return pubRepo.findAll();
+    }
+}
+
+interface PubRepo extends JpaRepository<Pub, Integer> {
 }
